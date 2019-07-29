c818acf04e98539ca824d3280b635e456a45d2ec
Lucinda <46454388+LucindaE@users.noreply.github.com>
2019-07-29 09:40:55 +0100

    Add files via upload

---
+++ 04072019_tortuosityPY_2.py
@@ -0,0 +1,50 @@
+# @CommandService cs
+# @DisplayService ds
+
+import os
+import csv
+from ij import IJ
+
+def getTortuosity(directory, imageName):
+	print(imageName)
+	image = IJ.openImage(directory+imageName)
+	
+	skeletoniseWrapper = cs.run("org.bonej.wrapperPlugins.SkeletoniseWrapper", False, ["inputImage",image])
+	skeletonInstance = skeletoniseWrapper.get()
+	skeleton = skeletonInstance.getOutput("skeleton")
+	IJ.save(skeleton, directory+"Skeletons/"+imageName.replace(".tif","")+"-skeleton")
+	
+	wrapper = cs.run("org.bonej.wrapperPlugins.AnalyseSkeletonWrapper", False, ["inputImage",image,"verbose",True])
+	wrapperInstance = wrapper.get()
+	
+	table = wrapperInstance.getOutput("verboseTable")
+	print(table[0])
+	nskeletons = max(table[0])
+	print(nskeletons)
+	branchLength = table[2]
+	euclideanDistance = table[9]
+	if(len(branchLength)>nskeletons):
+		print("multiple branches in skeleton: returning zero tortuosity")
+		return [0,0,0]
+	avgBL = sum(branchLength) / float(nskeletons)
+	avgED = sum(euclideanDistance)/float(nskeletons)
+	print("branch length: ", avgBL)
+	print("euclidean distance: ", avgED)
+	tortuosity = avgED/avgBL
+	return [avgBL, avgED, tortuosity]
+
+
+directory = "F:/Mouse A I13 scan/Tortuosity_100VoxSW_childvolumeROItest/Articular_surface/"
+os.chdir(directory)
+outputFile = open("tortuosity.csv",'w')
+outputWriter = csv.writer(outputFile, delimiter=',')
+outputWriter.writerow(["name","branchLength","euclideanDistance","tortuosity"])
+for fileName in os.listdir(directory):
+    if fileName.endswith(".tif"):
+    	t = getTortuosity(directory,fileName)
+    	print(t)
+    	print(type(t))
+    	row = [fileName,t[0],t[1],t[2]]
+    	outputWriter.writerow(row)
+    	IJ.run("Close All")
+outputFile.close()
---
+++ 08072019_tortuosityIJM_acellullar_SWROI.ijm
@@ -0,0 +1,61 @@
+
+run("Close All");
+//directory = "F:\Mouse A I13 scan\Tortuosity_100VoxSW_childvolumeROItest\";
+directory = getDirectory("Choose a Directory");
+fileList = getFileList(directory)
+print(fileList.length)
+for(i=0; i<fileList.length; i++) 
+{
+	if(!endsWith(fileList[i], ".bmp")) {
+		continue;
+	}
+	name = fileList[i];
+	open(name);
+	
+	
+	setAutoThreshold("Default dark");
+	setThreshold(1, 255);
+	setOption("BlackBackground", true);
+	run("Convert to Mask");
+	
+	run("Find Edges");
+	
+	setForegroundColor(0, 0, 0);
+	lines = getNumber("Please enter number of lines needed from this image (max 2): ", 1);
+
+	for(line=1;line<=lines;line++)
+	{
+		waitForUser("Line "+line+". Please remove lower line, and any other lines not needed right now, then click OK");	
+		run("Select None");
+		run("Dilate");
+		run("Purify", "labelling=Mapped chunk=4");
+		run("Invert LUT");
+		run("Dilate");
+		run("Dilate");
+		run("Dilate");
+		run("Dilate");
+		run("Dilate");
+		run("Dilate");
+		if(line==1){
+			open(fileList[i]);
+	
+			setAutoThreshold("Default dark");
+			setThreshold(1, 255);
+			setOption("BlackBackground", true);
+			run("Convert to Mask");
+			
+			run("Find Edges");
+			
+			setForegroundColor(0, 0, 0);
+		}
+	}
+	if(lines>1){
+		imageCalculator("Add create", name,replace(name, ".bmp", "-1.bmp"));
+		selectWindow("Result of "+name);
+	}
+	else {
+		selectWindow(name);
+	}
+	saveAs(directory+"Articular_surface/"+replace(name, ".bmp", "")+"_articular-surface.tif");
+	run("Close All");
+}
